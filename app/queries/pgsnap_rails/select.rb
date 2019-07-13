9af9defed695c530b1e234fc1cb327dbf22184b8
David Chin <dlcmhd@me.com>
2019-07-13 21:52:35 +0800

    all

--- app/queries/pgsnap_rails/select.rb
+++ app/queries/pgsnap_rails/select.rb
@@ -1,6 +1,6 @@
 module PgsnapRails
   class Select
-    attr_reader :node, :tree
+    attr_reader :built_sql, :node, :results, :retrieval_done, :tree
 
     def initialize
       @tree = {}
@@ -13,11 +13,29 @@ module PgsnapRails
       @tree[node_type.to_sym] = node
     end
 
+    def all
+      @built_sql = 'select 1'
+      retrieve_results_from_database
+      @retrieval_done = true
+      results.to_a
+    end
+
     def to_s
       tree
     end
 
     private
+
+    def results_retrieval_class_name
+      self.class.name.demodulize.underscore
+    end
+
+    def retrieve_results_from_database
+      return if retrieval_done
+      # build_missing_from_clause
+      # build_missing_select_command
+      @results = Pg::Results.retrieve(built_sql, results_retrieval_class_name)
+    end
 
     def validate(node_type)
       return if node.class.name.demodulize == node_type
