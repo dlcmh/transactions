b5a2d81600dc24dff347cd6c6d34be96e5910620
David Chin <dlcmhd@me.com>
2019-07-13 21:34:28 +0800

    Select

---
+++ app/queries/pgsnap_rails/select.rb
@@ -0,0 +1,27 @@
+module PgsnapRails
+  class Select
+    attr_reader :item, :items
+
+    def initialize
+      @items = []
+    end
+
+    def add(item)
+      @item = item
+      validate
+      @items << item
+    end
+
+    def to_s
+      return unless items.present?
+      [:SELECT, items.join(', ')].join(' ')
+    end
+
+    private
+
+    def validate
+      return if item.class.name.demodulize == 'SelectItem'
+      raise ArgumentError, 'not a valid SelectItem object'
+    end
+  end
+end
--- app/queries/pgsnap_rails/select_item.rb
+++ app/queries/pgsnap_rails/select_item.rb
@@ -5,15 +5,7 @@ module PgsnapRails
 
     def initialize(*args)
       @args = args
-      if args.size == 1
-        expression__params
-      elsif args.size == 2
-        output_name__expression__params
-      else
-        raise ArgumentError, 'Expected (1) column expression, or (2) alias, column expression'
-      end
-      @output_name = output_name
-      @expression = expression
+      validate
     end
 
     def to_s
@@ -22,16 +14,26 @@ module PgsnapRails
 
     private
 
-    def output_name__formatted
-      %(AS "#{output_name}") if output_name
-    end
-
     def expression__params
       @expression, = args
     end
 
     def output_name__expression__params
       @output_name, @expression = args
+    end
+
+    def output_name__formatted
+      %(AS "#{output_name}") if output_name
+    end
+
+    def validate
+      if args.size == 1
+        expression__params
+      elsif args.size == 2
+        output_name__expression__params
+      else
+        raise ArgumentError, 'Expected (1) column expression, or (2) alias, column expression'
+      end
     end
   end
 end
